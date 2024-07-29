96c9b551d81673be1d1287f05c5c63ced3ad0b28
KEAGAN-HC <165735259+KEAGAN-HC@users.noreply.github.com>
2024-07-29 02:22:52 -0500

    Update config.php

--- config/config.php
+++ config/config.php
@@ -2,7 +2,7 @@
 // Define tus constantes
 define('DB_HOST', 'unifeast.mysql.database.azure.com');
 define('DB_USER', 'ryanhers11yh');
-define('DB_PASS', 'APOLLo01?');
+define('DB_PASS', 'Apollo01');
 define('DB_NAME', 'card');
 define('CLIENT_ID', '');  // Puedes eliminar esto si no es necesario
 define('LOCALE', 'es_ES');
