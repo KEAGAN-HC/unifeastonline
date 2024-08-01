626ea8e782314b661ab410def85411de045c8e19
KEAGAN-HC <165735259+KEAGAN-HC@users.noreply.github.com>
2024-08-01 07:49:43 -0500

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,14 +1,21 @@
-<?php
-    $host = "localhost";
-    $user = "root";
-    $clave = "";
-    $bd = "card";
-    $conexion = mysqli_connect($host,$user,$clave,$bd);
-    if (mysqli_connect_errno()){
-        echo "No se pudo conectar a la base de datos";
-        exit();
-    }
-    mysqli_select_db($conexion,$bd) or die("No se encuentra la base de datos");
-    mysqli_set_charset($conexion,"utf8");
 
-    
+<?php
+$host = "unifeast.mysql.database.azure.com";
+$user = "ryanhers11yh";
+$clave = "Apollo01";
+$bd = "card";
+
+// Inicializa la conexión
+$conexion = mysqli_init();
+
+// Realiza la conexión sin SSL
+if (!mysqli_real_connect($conexion, $host, $user, $clave, $bd, 3306)) {
+    echo "No se pudo conectar a la base de datos: " . mysqli_connect_error();
+    exit();
+}
+
+// Selecciona la base de datos
+mysqli_select_db($conexion, $bd) or die("No se encuentra la base de datos");
+
+// Establece el conjunto de caracteres
+mysqli_set_charset($conexion, "utf8");
